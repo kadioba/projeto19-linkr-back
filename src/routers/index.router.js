ae5f2c390cf70dbef59ac0a38178324097bbb668
expeditus303 <ricardotonietto@gmail.com>
2023-05-30 18:06:44 -0300

    feat: get trending hashtags

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import usersRouter from "./users.router.js";
 import postRouter from "./post.routes.js";
+import hashtagsRouter from "./hashtags.router.js";
 
 const router = Router();
 
 router.use(usersRouter);
 router.use(postRouter)
+router.use(hashtagsRouter)
 router.all("*", (_req, res) => res.status(404).send({ message: "Not Found" }));
 
 export default router;
