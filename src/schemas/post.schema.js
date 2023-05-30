ba98f2f87b6ddb3b122900df000f53376278f68e
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-30 11:06:47 -0300

    fix: correcao do shcema de post

--- src/schemas/post.schema.js
+++ src/schemas/post.schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const publishPost = joi.object({
     url: joi.string().trim().uri({ scheme: ["http", "https"] }).required(),
-    content: joi.string().trim().required()
+    content: joi.string().trim()
 });
 
 const postSchema = { publishPost };
