2d6345e396b3eb44a9bb7c0c5e7365c1ed09fd7f
cfigueiroa <cfigueiroa@gmail.com>
2023-05-31 11:37:24 -0300

    feat: update database model

--- migrations/007_alter_tables_add_constrains.sql
+++ migrations/007_alter_tables_add_constrains.sql
@@ -1,14 +1,10 @@
+ALTER TABLE "posts"
+ADD
+    CONSTRAINT "posts_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
 ALTER TABLE "sessions"
 ADD
     CONSTRAINT "sessions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-
-ALTER TABLE "users_posts"
-ADD
-    CONSTRAINT "users_posts_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-
-ALTER TABLE "users_posts"
-ADD
-    CONSTRAINT "users_posts_fk1" FOREIGN KEY ("post_id") REFERENCES "posts"("id");
 
 ALTER TABLE "posts_hashtags"
 ADD
