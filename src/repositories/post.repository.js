b2892196db03a0e0b09a42e359eebd3129f0bd7a
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-31 13:45:25 -0300

    feat: scrap do url

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -1,11 +1,17 @@
 import db from "../configs/database.connection.js";
 
-async function createPost({ url, content, userId }) {
-    return await db.query(`INSERT INTO posts (url, content, user_id) VALUES ($1, $2, $3);`, [url, content, userId]);
+async function createPost({ url, content, userId }, data) {
+    console.log(data);
+    return await db.query(`INSERT INTO posts (url, content, user_id, url_title, url_description, url_picture )
+     VALUES ($1, $2, $3, $4, $5, $6);`, [url, content, userId, data.url_title, data.url_description, data.url_picture]);
 }
 
 async function getPosts() {
-    return await db.query(`SELECT * FROM posts ORDER BY created_at DESC LIMIT 20;`);
+    return await db.query(`SELECT posts.id, posts.content, posts.url, posts.created_at, posts.url_title, posts.url_description, posts.url_picture,
+    users.picture, users.username
+    FROM posts
+    JOIN users ON posts.user_id = users.id
+    ORDER BY created_at DESC LIMIT 20;`);
 }
 
 const postRepository = { createPost, getPosts };
